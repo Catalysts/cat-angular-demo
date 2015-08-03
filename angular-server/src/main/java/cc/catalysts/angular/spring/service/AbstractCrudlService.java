9689c208abdb9d6bd290a5133543ff07891764bc
Thomas Scheinecker <thomas.scheinecker@catalysts.cc>
2015-08-03 14:36:27 +0200

    add generic support for lists and change implementation to utilize conversion service

--- angular-server/src/main/java/cc/catalysts/angular/spring/service/AbstractCrudlService.java
+++ angular-server/src/main/java/cc/catalysts/angular/spring/service/AbstractCrudlService.java
@@ -2,13 +2,21 @@ package cc.catalysts.angular.spring.service;
 
 import cc.catalysts.angular.spring.core.Identifiable;
 import cc.catalysts.angular.spring.core.SearchRequest;
+import cc.catalysts.angular.spring.dto.PageDto;
 import cc.catalysts.angular.spring.exception.NotFoundException;
-import org.springframework.core.convert.converter.Converter;
+import cc.catalysts.angular.spring.util.SearchUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.GenericTypeResolver;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Klaus Lehner
@@ -20,12 +28,25 @@ public abstract class AbstractCrudlService<
         DETAIL_DTO,
         SEARCH_REQUEST extends SearchRequest> implements CrudlService<ID, LIST_DTO, DETAIL_DTO, SEARCH_REQUEST> {
 
-    protected final JpaRepository<ENTITY, ID> jpaRepository;
-    protected final Converter<ENTITY, DETAIL_DTO> dtoConverter;
+    private static final Logger LOG = LoggerFactory.getLogger(AbstractCrudlService.class);
 
-    public AbstractCrudlService(JpaRepository<ENTITY, ID> jpaRepository, Converter<ENTITY, DETAIL_DTO> dtoConverter) {
+    protected final JpaRepository<ENTITY, ID> jpaRepository;
+    protected final ConversionService conversionService;
+
+    private final Class<ENTITY> entityType;
+    private final Class<LIST_DTO> listType;
+    private final Class<DETAIL_DTO> detailType;
+
+    @SuppressWarnings("unchecked")
+    public AbstractCrudlService(JpaRepository<ENTITY, ID> jpaRepository, ConversionService conversionService) {
         this.jpaRepository = jpaRepository;
-        this.dtoConverter = dtoConverter;
+        this.conversionService = conversionService;
+
+        final Class<?>[] localParameters = GenericTypeResolver.resolveTypeArguments(
+                getClass(), AbstractCrudlService.class);
+        entityType = (Class<ENTITY>) localParameters[0];
+        listType = (Class<LIST_DTO>) localParameters[1];
+        detailType = (Class<DETAIL_DTO>) localParameters[2];
     }
 
     @Override
@@ -38,8 +59,32 @@ public abstract class AbstractCrudlService<
             if (!canRead(data)) {
                 throw new AccessDeniedException("");
             }
-            return dtoConverter.convert(data);
+            return conversionService.convert(data, detailType);
         }
+    }
+
+    @Override
+    public final PageDto<LIST_DTO> list(final SEARCH_REQUEST searchRequest) {
+        return doList(searchRequest);
+    }
+
+    protected LIST_DTO convertForList(ENTITY entity) {
+        return conversionService.convert(entity, listType);
+    }
+
+    protected DETAIL_DTO convertForDetail(ENTITY entity) {
+        return conversionService.convert(entity, detailType);
+    }
+
+    protected PageDto<LIST_DTO> doList(final SEARCH_REQUEST searchRequest) {
+        final Page<ENTITY> page = jpaRepository.findAll(SearchUtils
+                .toPageRequest(searchRequest));
+
+        final List<LIST_DTO> resultList = page.getContent().stream()
+                .map(this::convertForList)
+                .collect(Collectors.toList());
+
+        return new PageDto<>(resultList, page.getTotalElements());
     }
 
     protected abstract boolean canRead(ENTITY entity);
@@ -61,7 +106,27 @@ public abstract class AbstractCrudlService<
         entity = mergeFromDto(fghData, entity);
         entity = jpaRepository.save(entity);
 
-        return dtoConverter.convert(entity);
+        return convertForDetail(entity);
+    }
+
+
+    @Override
+    @Transactional(readOnly = false)
+    public DETAIL_DTO create(DETAIL_DTO fghData) {
+        ENTITY entity;
+        try {
+            entity = entityType.newInstance();
+        } catch (final InstantiationException | IllegalAccessException e) {
+            LOG.error(e.getMessage(), e);
+            throw new IllegalStateException(e);
+        }
+        if (!canUpdate(entity)) {
+            throw new AccessDeniedException("");
+        }
+        entity = mergeFromDto(fghData, entity);
+        entity = jpaRepository.save(entity);
+
+        return convertForDetail(entity);
     }
 
     protected abstract ENTITY mergeFromDto(DETAIL_DTO dto, ENTITY entity);
